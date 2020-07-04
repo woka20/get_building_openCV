36e560b5d3b45fa1241a5fca5548030df288a1eb
Woka Aditama <woka@Wokas-MacBook-Air.local>
2020-07-04 07:11:06 +0700

    done

--- getting_HSV_from_BGR.py
+++ getting_HSV_from_BGR.py
@@ -22,13 +22,13 @@ maps=cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 gray=cv2.cvtColor(maps, cv2.COLOR_RGB2HSV)
 
-cream = (float(color_codes[0]), float(color_codes[1]), float(color_codes[2]))
-brown = (float(color_codes[0])+30, float(color_codes[1])+30, float(color_codes[2])+30)
+brown = (float(color_codes[0]), float(color_codes[1]), float(color_codes[2]))
+darker_brown = (float(color_codes[0])+30, float(color_codes[1])+30, float(color_codes[2])+30)
 
 # cream= (13, 17, 216)
 # brown = (100, 100, 226)
 
-mask = cv2.inRange(gray, cream,brown)
+mask = cv2.inRange(gray, brown,darker_brown)
 hsv=cv2.bitwise_and(image,image, mask=mask)
 
 #show the final image
